b569dd74d816cdcf4353b66be9e10a08395fe23e
K MacLeod <ksmacleod99@gmail.com>
2021-08-02 08:19:32 -0400

    add tip calculator challenge

---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,17 @@
+//validate that the bill input is a number
+/* function validate() {
+    var numbers = /\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})/g;
+} */
+
+
+//click handler for tip amt buttons
+
+//Anyone paying with a credit card should tip on their portion of the total bill; https://www.realsimple.com/work-life/money/money-etiquette/tipping-etiquette-guide
+
+//divide bill by people
+
+//print to screen
+
+//apply tip amount to portion
+
+//print to screen
