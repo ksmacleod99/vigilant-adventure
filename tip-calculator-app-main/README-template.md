97d0185f8b3f737fd52e04184b5a43393831e293
K MacLeod <ksmacleod99@gmail.com>
2021-08-05 11:44:35 -0400

    started calcuation function. need help

--- tip-calculator-app-main/README-template.md
+++ tip-calculator-app-main/README-template.md
@@ -4,17 +4,19 @@ This is a solution to the [Tip calculator app challenge on Frontend Mentor](http
 
 ## Table of contents
 
-- [Overview](#overview)
-  - [The challenge](#the-challenge)
-  - [Screenshot](#screenshot)
-  - [Links](#links)
-- [My process](#my-process)
-  - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
-  - [Useful resources](#useful-resources)
-- [Author](#author)
-- [Acknowledgments](#acknowledgments)
+- [Frontend Mentor - Tip calculator app solution](#frontend-mentor---tip-calculator-app-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+    - [Continued development](#continued-development)
+    - [Useful resources](#useful-resources)
+  - [Author](#author)
+  - [Acknowledgments](#acknowledgments)
 
 **Note: Delete this note and update the table of contents based on what sections you keep.**
 
@@ -62,27 +64,7 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 
 ### What I learned
 
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
-
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
-```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
-}
-```
-
-If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
-
-**Note: Delete this note and the content within this section and replace with your own learnings.**
+- I can do client-side validation in vanilla HTML! To some extent.
 
 ### Continued development
 
--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,21 +1,50 @@
 //validate that the bill input is a number
-/* function validate() {
-    var numbers = /\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})/g;
-} */
+function validateBill() {
+    var numbers = /^\d+(.\d{1,2})?$/g;
+    var bill = document.getElementById("bill");
+    if (!bill.value.match(numbers)) {
+        document.getElementById("billError").classList.remove('hidden');
+        return false;
+    } else {
+        document.getElementById("billError").classList.add('hidden');
+        return true;
+    }
+}
 
+//validate tip amt is selected
+function getTip(amt){
+    return amt;
+    //TO DO: how to tell user that they need to pick a tip amount if not using custom amt? 
+    //TO DO: move `custom` validation here
+}
 
-//click handler for tip amt buttons
-//get ID
-//set ID as variable
-//button stays lit
-//
+//validate people input is a number
+function validatePeeps() {
+    var numbers = /^[1-9]+$/;
+    var peeps = document.getElementById("people");
+    if (!peeps.value.match(numbers)) {
+        document.getElementById("peopleError").classList.remove('hidden');
+        return false;
+      } else {
+        document.getElementById("peopleError").classList.add('hidden');
+        return true;
+    }
+}
 
-//Anyone paying with a credit card should tip on their portion of the total bill; https://www.realsimple.com/work-life/money/money-etiquette/tipping-etiquette-guide
+//CALCULATE Anyone paying with a credit card should tip on their portion of the total bill; https://www.realsimple.com/work-life/money/money-etiquette/tipping-etiquette-guide
+function calculate() {
+    var bill = document.getElementById("bill").value;
+    var perCent = getTip();
+    var people = document.getElementById("people").value;
 
-//divide bill by people
-
-//print to screen
-
-//apply tip amount to portion
+    //bill per person
+    var perPerson = (bill / people);
+        console.log("Bill per person is " + perPerson)
+    // tip
+    var tip = (perPerson * perCent);
+        console.log("Tip per person is " + tip)
+    //total
+    var total = ("Total per person is " + perPerson + tip);
+}
 
 //print to screen
