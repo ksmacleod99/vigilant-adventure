cfe690bc1c51e16d2f3a4f52596a0c174b5425c7
K MacLeod <ksmacleod99@gmail.com>
2021-08-10 17:02:28 -0400

    tip calc to FE index

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,3 +1,4 @@
+
 //validate that the bill input is a number
 function validateBill() {
     var numbers = /^\d+(.\d{1,2})?$/g;
@@ -11,13 +12,19 @@ function validateBill() {
     }
 }
 
-let perCent = '';
+//change selected tip button color
+function changeColor(amt){
+    document.getElementById(amt).classList.replace('tipAmt', 'active');
+}
+
+let percent = '';
+
 //validate tip amt is selected
 function getTip(amt){
+    changeColor(amt);
     //console.log(amt);
-    perCent = amt;
-    //TO DO: how to tell user that they need to pick a tip amount if not using custom amt? 
-    //TO DO: move `custom` validation here
+    percent = Number((amt/100).toFixed(2));
+
 }
 
 function getCustom(){
@@ -25,12 +32,10 @@ function getCustom(){
     var amt = document.getElementById("custom").value;
     if (!amt.match(numbers)) {
         document.getElementById("customError").classList.remove('hidden');
-        return false;
     } else {
         document.getElementById("customError").classList.add('hidden');
-        perCent = amt;
-        //return true;
-        console.log(perCent);
+        percent = Number((amt/100).toFixed(2));
+        console.log(percent);
     }
 
 }
@@ -46,17 +51,23 @@ function validatePeeps() {
         document.getElementById("peopleError").classList.add('hidden');
         return true;
     }
-}
+} 
+
+
 
 //CALCULATE Anyone paying with a credit card should tip on their portion of the total bill; https://www.realsimple.com/work-life/money/money-etiquette/tipping-etiquette-guide
-function calculate() {
+function calculate(amt) {
+   // var validateBll = /^\d+(.\d{1,2})?$/g;
+   // var validateCustom = /\d{1,2}$/;
+    //var validatePeople = /^[1-9]+$/;
     var bill = document.getElementById("bill").value;
+   // var custom = document.getElementById("custom").value;
     var people = document.getElementById("people").value;
 
     //bill per person
     var perPerson = (bill / people);
     // tip per person
-    var tip = Math.round((perPerson * perCent) * 100) / 100 ;
+    var tip = Math.round((perPerson * percent) * 100) / 100 ;
     //total per person
     var total =  (perPerson + tip).toFixed(2);
 
